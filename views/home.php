2230f904e8ec24c17d04ce3eeabdf2d03874fa25
Michael B. Bañaria <mbbmichaelbolobanaria25@gmail.com>
2025-06-17 16:49:27 +0800

    Alter navigation

--- views/home.php
+++ views/home.php
@@ -1,2 +1,24 @@
-<h1><?= $pageTitle ?></h1>
-<p><?= $message ?></p>
+<div class="container mt-5">
+    <div class="row">
+        <div class="col-6">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                        card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-6">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                        card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
--- views/partials/navbar.php
+++ views/partials/navbar.php
@@ -1,4 +1,12 @@
-<nav>
-    <a href="?page=home" hx-boost="true">Home</a> |
-    <a href="?page=about" hx-boost="true">About</a>
-</nav>
+<div class="d-flex justify-content-center">
+    <ul class="nav nav-underline">
+        <li class="nav-item">
+            <a class="nav-link <?= $_GET['page'] == 'home' ? 'active' : '' ?>" aria-current="page" href="?page=home"
+                hx-boost="true">Home</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link <?= $_GET['page'] == 'about' ? 'active' : '' ?>" href="?page=about"
+                hx-boost="true">About</a>
+        </li>
+    </ul>
+</div>
